bd2d1d7e5b1a23d84a3a684d7fef09c55b4745d5
Sargis <saq2121y@gmail.com>
2021-05-07 07:17:03 -0800

    added list

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <to-do v-on:save="addList"></to-do>
+    <p>{{list}}</p>
+  </div>
+</template>
+
+<script>
+import ToDo from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  props:{
+    list:String,
+  },
+  components: {
+    ToDo,
+  },
+  methods:{
+    addList(val) {
+    this.list = val;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+     <input type="text" v-model="text">
+     <button v-on:click="add">addText</button>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+      text:String,
+  },
+  methods:{
+    add() {
+      this.$emit('save',this.text)
+  }
+},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
