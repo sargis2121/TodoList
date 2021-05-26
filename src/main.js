375dc0ca9314a7742bf73b11c3a5c73f4bc5feb1
sargis2121 <saq2121y@gmail.com>
2021-05-26 06:07:31 -0800

    deleting bootstrap

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store.js'
-import { BootstrapVue } from 'bootstrap-vue'
-
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+
 new Vue({
   store,
   render: h => h(App)
